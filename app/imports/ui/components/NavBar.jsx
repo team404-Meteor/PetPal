28f2e2fa57cb12fb33706b242734e3369126248a
darleneagbayani <31275691+darleneagbayani@users.noreply.github.com>
2021-10-15 22:45:15 -1000

    navbar complete

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
-import { withRouter, NavLink } from 'react-router-dom';
+import { withRouter, NavLink, Link } from 'react-router-dom';
 import { Menu, Dropdown, Header } from 'semantic-ui-react';
 import { Roles } from 'meteor/alanning:roles';
 import 'bootstrap/dist/css/bootstrap';
@@ -49,14 +49,43 @@ class NavBar extends React.Component {
           <img className="mx-4" src="/images/logo.png" width="auto" height="auto" alt="logo" />
         </a>
 
-        <ul className="navbar-side" id="navbarSide">
-          <li className="navbar-side-item">
-            <a href="#" className="side-link">Sign In</a>
-          </li>
-          <li className="navbar-side-item">
-            <a href="#" className="side-link">Sign Up</a>
-          </li>
-        </ul>
+        {this.props.currentUser ? (
+          [<ul className="navbar-side" id="navbarSide">
+            <li className="navbar-side-item">
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-door-open" viewBox="0 0 16 16" color="#5f5f5f">
+                <path d="M8.5 10c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1z" />
+                <path d="M10.828.122A.5.5 0 0 1 11 .5V1h.5A1.5 1.5 0 0 1 13 2.5V15h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V1.5a.5.5 0 0 1 .43-.495l7-1a.5.5 0 0 1 .398.117zM11.5 2H11v13h1V2.5a.5.5 0 0 0-.5-.5zM4 1.934V15h6V1.077l-6 .857z" />
+              </svg>
+              <Link to="/list" className="side-link">Profile</Link>
+            </li>
+            <li className="navbar-side-item">
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16" color="#5f5f5f">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+              </svg>
+              <Link to="/signout" className="side-link" to="/signout">Sign Out</Link>
+            </li>
+          </ul>]
+        ) : ''}
+
+
+        {this.props.currentUser === '' ? ([<ul className="navbar-side" id="navbarSide">
+            <li className="navbar-side-item">
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-door-open" viewBox="0 0 16 16" color="#5f5f5f">
+                <path d="M8.5 10c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1z" />
+                <path d="M10.828.122A.5.5 0 0 1 11 .5V1h.5A1.5 1.5 0 0 1 13 2.5V15h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V1.5a.5.5 0 0 1 .43-.495l7-1a.5.5 0 0 1 .398.117zM11.5 2H11v13h1V2.5a.5.5 0 0 0-.5-.5zM4 1.934V15h6V1.077l-6 .857z" />
+              </svg>
+              <Link to="/signin" className="side-link">Sign In</Link>
+            </li>
+            <li className="navbar-side-item">
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16" color="#5f5f5f">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+              </svg>
+              <Link to="/signin" className="side-link" to="/signup">Sign Up</Link>
+            </li>
+          </ul>]
+        ) : ''}
 
         <div className="overlay"></div>
 
