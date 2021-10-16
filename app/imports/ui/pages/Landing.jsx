37bbde199398b3fe46fb78c3efa354dd9bbd0dcf
darleneagbayani <31275691+darleneagbayani@users.noreply.github.com>
2021-10-15 23:40:26 -1000

    edits to landing

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,52 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import 'bootstrap/dist/css/bootstrap';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-      <Grid id='landing-page' verticalAlign='middle' textAlign='center' container>
-
-        <Grid.Column width={4}>
-          <Image size='small' circular src="/images/meteor-logo.png"/>
-        </Grid.Column>
-
-        <Grid.Column width={8}>
-          <h1>Welcome to this template</h1>
-          <p>Now get to work and modify this app!</p>
-        </Grid.Column>
-
-      </Grid>
+      <div className="container py-2 my-5">
+        <div className="row px-kg-5 py-lg-5 my-lg-5">
+          <div className="col-md-2 col-12 text-center mx-auto text-center h-100">
+            <div class="row pb-5 justify-content-center">
+              <div class="col mx-auto">
+                <img src="/images/home-dog.png" className="img-fluid"></img>
+              </div>
+            </div>
+            <div class="row pb-5 justify-content-center">
+              <div class="col text-center">
+                VIEW PETS
+              </div>
+            </div>
+          </div>
+          
+          <div className="col-md-2 col-12 text-center mx-auto text-center h-100">
+            <div class="row pb-5 justify-content-center">
+              <div class="col mx-auto">
+                <img src="/images/home-bunny1.png" className="img-fluid"></img>
+              </div>
+            </div>
+            <div class="row pb-5 justify-content-center">
+              <div class="col text-center">
+                LIST A PET
+              </div>
+            </div>
+          </div>
+          
+          <div className="col-md-2 col-12 text-center mx-auto text-center h-100">
+            <div class="row pb-5 justify-content-center">
+              <div class="col mx-auto">
+                <img src="/images/home-bunny.png" className="img-fluid"></img>
+              </div>
+            </div>
+            <div class="row pb-5 justify-content-center">
+              <div class="col text-center">
+                LEARN MORE
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
