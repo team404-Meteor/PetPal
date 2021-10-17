ad2cc624053101019d5086b6de83c86989548f43
Rexter Delos Santos <rexterds@hawaii.edu>
2021-10-17 00:28:46 -1000

    eslint fix on ListPets

--- app/imports/ui/pages/ListPets.jsx
+++ app/imports/ui/pages/ListPets.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 // import PropTypes from 'prop-types';
 import { Button, Form, Image, Segment, TransitionablePortal } from 'semantic-ui-react';
 import PetCard from '../components/PetCard';
