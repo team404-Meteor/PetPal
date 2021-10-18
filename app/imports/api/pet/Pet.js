90c7a1dd4dc6f2dcaeb25d5543c32896a6d457a6
Jonathan-Ma <jma66@hawaii.edu>
2021-10-17 19:23:43 -1000

    lone block removed

--- app/imports/api/pet/Pet.js
+++ app/imports/api/pet/Pet.js
@@ -1,7 +1,6 @@
 import { Mongo } from 'meteor/mongo';
 import SimpleSchema from 'simpl-schema';
 import { Tracker } from 'meteor/tracker';
-{/*import { ImageField } from 'uniforms-semantic';*/}
 
 /**
  * The PetCollection. It encapsulates state and variable values for stuff.
