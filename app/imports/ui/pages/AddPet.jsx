8d7301875a42e3d9677e4ba8ded256fcbe5a2685
Arslan Rakhmankulov <ArslanxR@gmail.com>
2021-10-18 02:38:11 -0700

    fixed styling

--- app/imports/ui/pages/AddPet.jsx
+++ app/imports/ui/pages/AddPet.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AutoForm, ErrorsField, SelectField, SubmitField, TextField } from 'uniforms-semantic';
+import { AutoForm, ErrorsField, HiddenField, BoolField, SelectField, SubmitField, TextField, LongTextField } from 'uniforms-semantic';
 import swal from 'sweetalert';
 import { Meteor } from 'meteor/meteor';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
@@ -24,11 +24,8 @@ const formSchema = new SimpleSchema({
     type: String,
   },
   status: {
-    type: String,
-    allowedValues: [
-      'Available',
-      'Adopted',
-    ],
+    type: Boolean,
+    defaultValue: true,
   },
   petType: {
     type: String,
@@ -78,31 +75,33 @@ class AddPet extends React.Component {
                 <div class="col-11 pt-1 text-center mx-auto">
                   <div class="row justify-content-center mx-auto">
                     <div class="col-lg-6 col-12 text-left pl-2">
-                      <TextField name='petName' />
+                      <TextField name='petName' class="custom-select w-100"/>
                     </div>
                     <div class="col-lg-6 col-12 text-left pl-2">
-                      <SelectField name='petType' class="custom-select w-100" />
+                      <SelectField name='petType' class="custom-select w-100" placeholderText=''/>
                     </div>
                   </div>
                   <div class="row pt-3 justify-content-center mx-auto">
                     <div class="col-6 text-left pl-2">
-                      <TextField name='breed' />
+                      <TextField name='breed' class="custom-select w-100"/>
                     </div>
                     <div class="col-6 text-left pl-2">
-                      <TextField name='age' />
+                      <TextField name='age' class="custom-select w-100"/>
                     </div>
                   </div>
                   <div class="row pt-3 justify-content-center mx-auto">
                     <div class="col-12 text-left pl-2">
-                      <TextField name='description' />
+                      <LongTextField name='description' class="custom-select w-100"/>
                     </div>
                   </div>
                   <div class="row pt-3 justify-content-center mx-auto">
-                    <div class="col-lg-6 col-12 text-left pl-2">
+                    <div class="col-lg-12 col-12 text-left pl-2">
                       <TextField name='photoUrl' />
                     </div>
-                    <div class="col-lg-6 col-12 text-left pl-2">
-                      <SelectField name="status" class="custom-select w-100" />
+                  </div>
+                  <div className="row pt-3 justify-content-center mx-auto">
+                    <div className="col-lg-6 col-12 text-left pl-2">
+                      <HiddenField name="status" class="custom-select w-100"/>
                     </div>
                   </div>
                   <div class="row pt-3 justify-content-center mx-auto">
