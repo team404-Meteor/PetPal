d6a677cb17b417daeb88614b5ab835265cc211e6
darleneagbayani <31275691+darleneagbayani@users.noreply.github.com>
2021-10-18 13:15:56 -1000

    update styling

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap';
 
+// test
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
+
   render() {
     return (
-<<<<<<< Updated upstream
-      <Grid id='landing-page' verticalAlign='middle' textAlign='center' container>
-
-        <Grid.Column width={4}>
-          <Image size='small' circular src="/images/meteor-logo.png"/>
-        </Grid.Column>
-
-        <Grid.Column width={8}>
-          <h1>Welcome to this template</h1>
-          <p>Now get to work and modify this app!</p>
-        </Grid.Column>
-
-      </Grid>
-=======
       <div className="container pt-lg-5 py-2 my-5 px-5">
         <div className="row py-lg-5 my-lg-5 justify-content-center">
           <div className="col-lg-4 col-md-7 col-12 text-center">
@@ -67,7 +56,6 @@ class Landing extends React.Component {
           </div>
         </div>
       </div>
->>>>>>> Stashed changes
     );
   }
 }
