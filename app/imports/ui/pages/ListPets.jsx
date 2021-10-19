6c33615a697d2f97d8c3f2484adddf7778ac5053
darleneagbayani <31275691+darleneagbayani@users.noreply.github.com>
2021-10-19 13:56:54 -1000

    fix pet  listing image size

--- app/imports/ui/pages/ListPets.jsx
+++ app/imports/ui/pages/ListPets.jsx
@@ -121,7 +121,7 @@ function ListPets({ petReady, pets }) {
                   if (Other && pet.petType === 'Other') return pet;
 
                 }).map((pet, index) => (
-                  <div key={index} className='col-lg-4 col-12 text-center'>
+                  <div key={index} className='col-lg-4 col-md-4 col-12 text-center'>
                     <PetCard pet={{ name: pet.petName, breed: pet.breed, age: pet.age, photoUrl: pet.photoUrl, _id: pet._id }}/>
                   </div>
                 ))
