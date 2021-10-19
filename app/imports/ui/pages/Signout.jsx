c1a8f45a5a6a3eb72f85ae9c07e795855f011628
Jonathan-Ma <jma66@hawaii.edu>
2021-10-18 20:52:34 -1000

    underlined "home" to standout

--- app/imports/ui/pages/Signout.jsx
+++ app/imports/ui/pages/Signout.jsx
@@ -9,19 +9,18 @@ export default class Signout extends React.Component {
     Meteor.logout();
     return (
       <div id="signout-page" className="container-fluid px-5 my-5">
-        <div class="row justify-content-center pt-5 mt-5">
-          <div class="col-8 text-center">
+        <div className="row justify-content-center pt-5 mt-5">
+          <div className="col-8 text-center">
             <img src="/images/goodbye.png"></img>
           </div>
-        <div class="row justify-content-center">
-          <div class="col-12 text-center pt-4">
-            <h1>You have signed out.</h1>
-            Return <Link to="/">Home.</Link>
-          </div>
+          <div className="row justify-content-center">
+            <div className="col-12 text-center pt-4">
+              <h1>You have signed out.</h1>
+            Return <Link to="/"><u>Home</u>.</Link>
+            </div>
           </div>
         </div>
       </div>
     );
   }
 }
-
